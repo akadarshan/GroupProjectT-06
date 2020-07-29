d165447eeae4c1898efd509ed163762e1996bd25
Darshan <darshanvadodariya71@gmail.com>
2020-07-29 21:47:58 +0530

    Splash screen create

--- app/src/main/java/com/example/groupprojectt_06/MainActivity.java
+++ app/src/main/java/com/example/groupprojectt_06/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.groupprojectt_06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent dsp = new Intent(MainActivity.this,login.class);
+                startActivity(dsp);
+                finish();
+            }
+        },2000);
     }
 }
---
+++ app/src/main/java/com/example/groupprojectt_06/login.java
@@ -0,0 +1,7 @@
+package com.example.groupprojectt_06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login extends AppCompatActivity {
+
+}
