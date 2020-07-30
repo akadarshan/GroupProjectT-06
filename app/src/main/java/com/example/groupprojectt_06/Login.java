1c1f82e7c68c4b5fa155f336799a7145f5336828
Mohit <mohitratanpara2001@gmail.com>
2020-07-30 18:34:47 +0530

    apply link to login button to move to contact activity and contact activity chenged to full screen activity(header removed).

--- app/src/main/java/com/example/groupprojectt_06/Login.java
+++ app/src/main/java/com/example/groupprojectt_06/Login.java
@@ -2,13 +2,29 @@ package com.example.groupprojectt_06;
 
         import androidx.appcompat.app.AppCompatActivity;
 
+        import android.content.Intent;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button btnlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnlogin = findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login.this,Contact.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
     }
 }
